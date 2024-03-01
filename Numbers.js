66ba6fec54803d933a6557f0ad3d2758c6d8ab7f
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-03-01 16:41:22 +0500

    Add files via upload

---
+++ Numbers.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log(6 + 2); // Addition
+console.log(10 - 2); // Substraction
+console.log(4 * 2); // Multiplication
+console.log(16 / 2); // Division
---
+++ Numbers.ts
@@ -0,0 +1,7 @@
+console.log(6+2); // Addition
+
+console.log(10-2); // Substraction
+
+console.log(4*2); // Multiplication
+
+console.log(16/2); // Division
